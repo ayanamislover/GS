99a9f5c2ca4c84204d41295c44ed48150edf6fe7
zohnwang907 <156605267+zohnwang907@users.noreply.github.com>
2024-02-25 02:34:02 +0000

    Add files via upload

--- macdjanggo/activityboard/urls.py
+++ macdjanggo/activityboard/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     #path('', views.activity_join, name='activity_join'),
-    path('',views.scan_qr_code,name='scan_qr_code'),
     path('activity/quiz/', views.quiz_home, name='quiz_home'),
     path('activity/qrcode/',views.generate_qrcode,name='qrcode'),
-    
+    path("activity/scanqrcode/",views.scan_qr, name='scan_qr')
 ]
--- macdjanggo/activityboard/views.py
+++ macdjanggo/activityboard/views.py
@@ -34,39 +34,7 @@ def generate_qrcode(request):
     # 将图像返回给客户端
     return HttpResponse(buffer.getvalue(), content_type="image/png")
 
-def scan_qr_code(request):
-    if request.method == 'POST':
-        uploaded_file = request.FILES.get('file')
-        if uploaded_file:
-            # 读取上传的图片文件数据
-            image_data = uploaded_file.read()
-            # 使用 OpenCV 读取图像
-            img = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_COLOR)
-            # 将图像转换为灰度图像
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            # 使用 OpenCV 识别 QR Code
-            detector = cv2.QRCodeDetector()
-            retval, decoded_info, points, straight_qrcode = detector.detectAndDecodeMulti(gray)
-            if retval:
-                # 如果返回值是元组，则遍历处理每个 QR Code
-                if isinstance(decoded_info, tuple):
-                    for qr_code_url in decoded_info:
-                        # 检查解析出的 URL 是否以 http:// 或 https:// 开头，如果不是，则添加相应的协议前缀
-                        if not qr_code_url.startswith('http://') and not qr_code_url.startswith('https://'):
-                            qr_code_url = 'http://' + qr_code_url
-                        # 将 URL 转换为字符串，并进行重定向
-                        return HttpResponseRedirect(str(qr_code_url))
-                else:
-                    # 如果返回值不是元组，则直接处理解析出的 URL
-                    qr_code_url = decoded_info
-                    # 检查解析出的 URL 是否以 http:// 或 https:// 开头，如果不是，则添加相应的协议前缀
-                    if not qr_code_url.startswith('http://') and not qr_code_url.startswith('https://'):
-                        qr_code_url = 'http://' + qr_code_url
-                    # 将 URL 转换为字符串，并进行重定向
-                    return HttpResponseRedirect(str(qr_code_url))
-            else:
-                return HttpResponseBadRequest('No QR Code found in uploaded image.')
-        else:
-            return HttpResponseBadRequest('No file uploaded.')
-    else:
-        return render(request, 'activity_join.html')
+
+    
+def scan_qr(request):
+    return render(request, 'scan_qr.html')
