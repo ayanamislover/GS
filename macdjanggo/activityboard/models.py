575585152741a0d112ddc9d1462eef6bdcd368fa
zohnwang907 <156605267+zohnwang907@users.noreply.github.com>
2024-02-28 23:24:37 +0000

    already connect other projects

--- macdjanggo/activityboard/models.py
+++ macdjanggo/activityboard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+# models.py
+from django.db import models
+
+
+
+
+
+class QuizSession(models.Model):
+    series_id = models.CharField(max_length=255,null=True)
+    player = models.CharField(max_length=155)
+    score = models.IntegerField(default=0)
+
+
