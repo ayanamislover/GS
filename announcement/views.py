89a00d1789b415578477b4250d0fcad8d43c0698
1otussssss <125210380+1otussssss@users.noreply.github.com>
2024-02-28 22:19:39 +0000

    Update views.py

--- announcement/views.py
+++ announcement/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Announcement  # 确保从你的模型文件中导入 Announcement
-
+from .models import Announcement  # Make sure to import Announcement from your models file
 
 def list(request):
     announcements = Announcement.objects.order_by('date_start')
     return render(request, 'announcement/list.html', {'announcements': announcements})
 
 def detail(request, title):
-    # 使用 get_object_or_404 来获取特定公告或返回404
+    # Use get_object_or_404 to get a specific announcement or return a 404 error
     announcement = get_object_or_404(Announcement, title=title)
     return render(request, "announcement/detail.html", {'announcement': announcement})
+
