903e83230fff8b7d91a94eae1417ab9501fd04cf
zohnwang907 <156605267+zohnwang907@users.noreply.github.com>
2024-02-28 23:37:36 +0000

    the activity  answer match （first sprint）
    contain the activity answer match and the result leaderboard

---
+++ activityboard/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.activity_join, name='activity_join'),
+    path('activity/leaderboard/<str:nickname>/<int:additional_score>/<int:series_id>/',views.quiz_leaderboard,name='quiz_leaderboard'),
+    #path('activity/leaderboard/<str:nickname>/',views.quiz_leaderboard,name='quiz_leaderboard'),
+    path('activity/qrcode/',views.generate_qrcode,name='qrcode'),
+    path("activity/scanqrcode/",views.scan_qr, name='scan_qr'),
+    path("activity/enternickname/<int:series_id>",views.enternickname,name="enternickname"),
+]
