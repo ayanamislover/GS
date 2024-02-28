20fc73c17b1f542bba70069b64477d80263a6d82
shuonanyang <148327687+shuonanyang@users.noreply.github.com>
2024-02-28 23:55:52 +0000

    Delete mysite/web directory

--- mysite/web/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class UserForm(forms.Form):
-    username = forms.CharField(label='用户名', max_length=50)
-    password = forms.CharField(label='密码', widget=forms.PasswordInput())
-    email = forms.EmailField(label='邮箱')
