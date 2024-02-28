ca769e61693e926a93985505d82031432400de91
ayanamislover <104753598+ayanamislover@users.noreply.github.com>
2024-02-28 20:29:01 +0000

    Ver 0.7 Improvement of annotations

--- navi/views.py
+++ navi/views.py
@@ -20,11 +20,9 @@ from django.http import HttpResponseRedirect
 # def can_start_new_game(nickname):
 #     player = PlayerProfile.nickname
 #     now = timezone.now()
-#     # 如果用户从未开始过游戏则可以开始新游戏
 #     if not PlayerProfile.last_game_start:
 #         return True
 #
-#         # 如果已经过去了7天，则可以开始新游戏
 #     if (datetime.now().date() - PlayerProfile.last_game_start) >= datetime.timedelta(days=7):
 #         return True
 #     elif (datetime.now().date() - PlayerProfile.last_game_start) < datetime.timedelta(
@@ -38,29 +36,27 @@ from django.http import HttpResponseRedirect
 
 def checkersgame(request, nickname):
     # user = PlayerProfile.nickname
-    # # 检查会话中是否已经存储了位置
     # if can_start_new_game(user):
-    #     # 重置用户的游戏信息
     #     user.last_game_start = timezone.now()
     #     user.verified_locations_count = 0
     # else:
     #     print('You are not allowed to start a new game yet.')
-
-
     if 'target_id' not in request.session:
-        # 如果没有存储，则随机选择一个新位置
+        # if not request.session.get('target_id'):
         count = Checker.objects.count()
+        # select a random location from the database
         random_index = random.randint(0, count - 1)
         print('Random:', random_index, 'Total count:', count)
-
+        # select a random location from the database
         target = Checker.objects.all()[random_index]
-        # 将选中的位置的ID存储在会话中
+        # store the location in the session
         request.session['target_id'] = target.id
     else:
-        # 如果已经存储了位置，就从会话中获取
+        # if the session already contains a location, retrieve it.
         target_id = request.session['target_id']
         target = Checker.objects.get(id=target_id)
 
+    # retrieve the latitude and longitude of the target location
     target_latitude = target.latitude
     target_longitude = target.longitude
     nickname = nickname
@@ -69,7 +65,7 @@ def checkersgame(request, nickname):
         'target_latitude': target_latitude,
         'target_longitude': target_longitude,
     }
-
+    # return the navigation page with the target location and the user's nickname.
     return render(request, 'navigation.html', context)
 
 
@@ -78,18 +74,22 @@ def checkersgame(request, nickname):
 def check_location(request, nickname):
     print('Request received:', request.body)
     data = json.loads(request.body.decode('utf-8'))
+    # collect the user's location and the target location from the request
     user_lat = data['user_latitude']
     user_lon = data['user_longitude']
     target_lat = data['target_latitude']
     target_lon = data['target_longitude']
     try:
-        # 检查用户是否在目标位置的阈值范围内
+
         if is_within_distance(user_lat, user_lon, target_lat, target_lon, threshold=11):
+            # If the user is within the target range, select a new random target and return a success message
             new_target = get_new_random_target(request.session.get('target_id'))
             request.session['target_id'] = new_target.id
             request.session.save()
             print('Random index:', request.session['target_id'])
+            # update the user's verified locations count
             redirect_url = reverse('ans:series_detail', kwargs={'series_id': 1, 'nickname': nickname})
+            # redirect to the answerquestion app page
             print('Check-in successful, redirecting to:', redirect_url)
             response_data = {
                 'status': 'success',
@@ -97,24 +97,24 @@ def check_location(request, nickname):
             }
             return JsonResponse(response_data)
         else:
-            # 如果用户不在目标范围内，返回失败消息
+            # If the user is not within the target range, return a failure message
             return JsonResponse({'status': 'failure', 'message': 'Check-in failed, out of range.'})
     except KeyError:
-        # 如果POST数据不完整，返回错误消息
+        # If the request does not contain the required data, return a failure message
         return JsonResponse({'status': 'failure', 'message': 'Incomplete data provided.'})
 
 
 def get_new_random_target(exclude_id):
-    # 获取除了exclude_id之外的所有Checker对象的ID列表
+    # Get a list of all the IDs in the database
     ids = Checker.objects.exclude(id=exclude_id).values_list('id', flat=True)
     ids_list = list(ids)
 
-    # 如果列表为空，返回None
+    # If the list is empty, return None
     if not ids_list:
         return None
 
-    # 从列表中随机选择一个ID
+    # Select a random ID from the list
     random_id = random.choice(ids_list)
 
-    # 根据随机选择的ID获取Checker对象
+    # Return the Checker object with the selected ID
     return Checker.objects.get(id=random_id)
