06897fe99e4603f0a0c4b4dae48bedd8024abd2a
cl1007lcq <147420498+cl1007lcq@users.noreply.github.com>
2024-03-17 15:08:39 +0000

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,9 +1,29 @@
-from django.urls import path
-from pictures import views
-from django.views.generic import RedirectView
+"""
+URL configuration for djangoProject1 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.generic import RedirectView
+
 from djangoProject1 import settings
+
 urlpatterns = [
-    path('', views.upload_view, name='upload_view'),  # 对应 photo 视图
-    path("api/upload", views.upload_api, name='upload_api'),
-]
+    path("admin/", admin.site.urls),
+    path("", include("pictures.urls")),
+    # 将根URL重定向到 /pictures/upload/
+    # re_path(r'^$', RedirectView.as_view(url='/pictures/upload/', permanent=False)),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
