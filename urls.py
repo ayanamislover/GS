8ba1fd7f4dbc1eae7f5d018882703f3b08a72798
cl1007lcq <147420498+cl1007lcq@users.noreply.github.com>
2024-03-17 15:07:42 +0000

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from pictures import views
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from djangoProject1 import settings
+urlpatterns = [
+    path('', views.upload_view, name='upload_view'),  # 对应 photo 视图
+    path("api/upload", views.upload_api, name='upload_api'),
+]
