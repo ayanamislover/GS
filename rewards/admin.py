4872b05357080cea8cc79f7d7c211eeb18c423ab
peterBenjamin <petergsq6306@gmail.com>
2024-03-18 13:07:12 +0000

    new

--- rewards/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib import admin
-from .models import Gift
-
-admin.site.register(Gift)
-
-# Register your models here.
--- rewards/urls.py
+++
@@ -1,9 +0,0 @@
-# rewards/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('store/', views.gift_list, name='gift_list'),
-    path('detail/<int:id>/', views.gift_detail, name='gift_detail'),
-]
