7d050efdf95ac842a6615feaeca6c113baf10066
cl1007lcq <147420498+cl1007lcq@users.noreply.github.com>
2024-03-19 15:31:47 +0000

    Add files via upload

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from pictures import views
-from django.views.generic import RedirectView
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('<str:nickname>/', views.upload_view, name='upload_view'),  # 对应 photo 视图
