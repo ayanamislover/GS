63a9a5f901ba4420b2f1a39445a0574a484bc089
ayanamislover <104753598+ayanamislover@users.noreply.github.com>
2024-03-21 12:38:21 +0000

    Update urls.py

--- web/urls.py
+++ web/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('logout/', views.logout_view, name='logout_view'),
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('regist/', views.regist, name='regist'),
     path('gdpr/',views.gdpr,name='gdpr'),
-    path('logout/', views.logout_view, name='logout_view'),
+    
 ]
