595b455538f48dee9afa75282cacaa9d047758c3
BenjaminGuo666 <156781268+BenjaminGuo666@users.noreply.github.com>
2024-03-23 22:43:16 +0000

    Update urls.py

--- pointshop/urls.py
+++ pointshop/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import shop, lottery_draw, buy_product, product_detail
 from .views import search_results
 urlpatterns = [
-    path('<str:nickname>/', shop, name='shop'),  # 确保这个URL模式指向了上面创建的视图函数
+    path('<str:nickname>/', shop, name='shop'),  
     path('search/', search_results, name='search_results'),
-    path('lottery/draw/', lottery_draw, name='lottery_draw'),  # 抽奖URL
+    path('lottery/draw/', lottery_draw, name='lottery_draw'),  # lottery URL
     path('buy/<str:name>/<str:nickname>/', buy_product, name='buy_product'),
     path('product/<str:name>/<str:nickname>/', product_detail, name='product_detail'),
 
