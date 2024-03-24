e94022318772cb2cfda5db8cba715806906af5e0
ayanamislover <104753598+ayanamislover@users.noreply.github.com>
2024-03-24 20:47:49 +0000

    Final version, valid only for this version

--- navi/urls.py
+++ navi/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('game/<str:nickname>', views.checkersgame, name='checkers_game'),
-    path('check_location/<str:nickname>', views.check_location, name='check_location'),
+    path('<str:nickname>', views.checkersgame, name='checkers_game'),
+    path('check_location/<str:nickname>/<str:tag>', views.check_location, name='check_location'),
+    path('cal_carbon/<str:nickname>', views.cal_carbon, name='cal_carbon'),
 ]
--- navi/views.py
+++ navi/views.py
@@ -1,46 +1,23 @@
 import json
 from django.shortcuts import render
 from .models import Checker, player_profile
+from usersinformation.models import PlayerProfile
 import random
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
-from .location_utils import is_within_distance
-import datetime
-from django.utils import timezone
+from .location_utils import is_within_distance, calculate_distance
 from django.urls import reverse
+from decorate import login_requiredforuser
+import os
 include = ('answerquestion.views', 'answerquestion', 'answerquestion.urls')
 from answerquestion.views import series_detail
-# from usersinformation.models import PlayerProfile
 from django.http import HttpResponseRedirect
+from pictures.views import upload_view
 
 
-# from answerquestion.views import series_detail2,results_page2
-
-# def can_start_new_game(nickname):
-#     player = PlayerProfile.nickname
-#     now = timezone.now()
-#     if not PlayerProfile.last_game_start:
-#         return True
-#
-#     if (datetime.now().date() - PlayerProfile.last_game_start) >= datetime.timedelta(days=7):
-#         return True
-#     elif (datetime.now().date() - PlayerProfile.last_game_start) < datetime.timedelta(
-#             days=7) and PlayerProfile.verified_locations_count == 3:
-#         print('You have already verified 3 locations this week. You can start a new game next week.')
-#         return False
-#     else:
-#         print('You have already finished a game this week. You can start a new game next week.')
-#     return False
-
-
+@login_requiredforuser
 def checkersgame(request, nickname):
-    # user = PlayerProfile.nickname
-    # if can_start_new_game(user):
-    #     user.last_game_start = timezone.now()
-    #     user.verified_locations_count = 0
-    # else:
-    #     print('You are not allowed to start a new game yet.')
     if 'target_id' not in request.session:
         # if not request.session.get('target_id'):
         count = Checker.objects.count()
@@ -60,18 +37,28 @@ def checkersgame(request, nickname):
     target_latitude = target.latitude
     target_longitude = target.longitude
     nickname = nickname
+    tag = target.tag
+    picture = target.picture
+    overview = target.overview
+    location_name = target.location_name
+    GMAK = os.environ.get('GMAK')
     context = {
         'nickname': nickname,
         'target_latitude': target_latitude,
         'target_longitude': target_longitude,
+        'tag': tag,
+        'picture': picture,
+        'overview': overview,
+        'GMAK': GMAK,
+        'loc': location_name
     }
-    # return the navigation page with the target location and the user's nickname.
+    # return the navigation page with the target location and the user's nickname in the context
     return render(request, 'navigation.html', context)
 
-
+@login_requiredforuser
 @csrf_exempt
 @require_http_methods(["POST"])
-def check_location(request, nickname):
+def check_location(request, nickname, tag):
     print('Request received:', request.body)
     data = json.loads(request.body.decode('utf-8'))
     # collect the user's location and the target location from the request
@@ -87,10 +74,13 @@ def check_location(request, nickname):
             request.session['target_id'] = new_target.id
             request.session.save()
             print('Random index:', request.session['target_id'])
-            # update the user's verified locations count
-            redirect_url = reverse('ans:series_detail', kwargs={'series_id': 1, 'nickname': nickname})
-            # redirect to the answerquestion app page
-            print('Check-in successful, redirecting to:', redirect_url)
+            # select the scenario based on the tag
+            if tag == 'question':
+                redirect_url = reverse('ans:series_detail', kwargs={'series_id': 2, 'nickname': nickname})
+            elif tag == 'meadow':
+                redirect_url = reverse('textGame:SceneSelect', kwargs={'loc_id': 1, 'nickname': nickname})
+            elif tag == 'photo':
+                redirect_url = reverse('pictures:upload_view', kwargs={'nickname': nickname})
             response_data = {
                 'status': 'success',
                 'redirect_url': redirect_url
@@ -102,6 +92,23 @@ def check_location(request, nickname):
     except KeyError:
         # If the request does not contain the required data, return a failure message
         return JsonResponse({'status': 'failure', 'message': 'Incomplete data provided.'})
+@login_requiredforuser
+@csrf_exempt
+def cal_carbon(request, nickname):
+    data = json.loads(request.body.decode('utf-8'))
+    # collect the user's location and the target location from the request
+    user_lat = data['user_latitude']
+    user_lon = data['user_longitude']
+    target_lat = data['target_latitude']
+    target_lon = data['target_longitude']
+    try:
+        player_profile = PlayerProfile.objects.get(nickname=nickname)
+        # Increase the carbon footprint of the user based on the distance between the user and the target
+        player_profile.carbon += calculate_distance(user_lat, user_lon, target_lat,target_lon) * 300
+        player_profile.save()
+        return JsonResponse({'status': 'success'})
+    except PlayerProfile.DoesNotExist:
+        pass
 
 
 def get_new_random_target(exclude_id):
