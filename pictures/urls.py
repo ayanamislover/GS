6a287cadaf2ee2ee8479720a376b9f974504c07b
cl1007lcq <147420498+cl1007lcq@users.noreply.github.com>
2024-03-24 21:19:40 +0000

    Delete pictures directory

--- pictures/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from pictures import views
-
-urlpatterns = [
-    path('<str:nickname>/', views.upload_view, name='upload_view'),  # 对应 photo 视图
-    path("api/upload", views.upload_api, name='upload_api'),
-]
